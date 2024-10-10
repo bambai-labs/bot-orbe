d9c3c1a00a141ff20ee51a0d3905ddbe47c04cd1
Jose Gaspar <jgasparlopez29@correo.unicordoba.edu.co>
2024-10-09 23:47:01 -0500

    initial

--- src/config/variables.js
+++ src/config/variables.js
@@ -12,8 +12,11 @@ export const MONGO_DB_NAME = process.env.MONGO_DB_NAME;
 export const OPENAI_API_KEY = process.env.OPENAI_API_KEY;
 export const ASSISTANT_ID = process.env.ASSISTANT_ID;
 
-export const CHATWOOT_ID = process.env.CHATWOOT_ID;
-export const CHATWOOT_URL = process.env.CHATWOOT_URL;
-
 export const ELEVENLABS_API_KEY = process.env.ELEVENLABS_API_KEY;
 export const ELEVENLABS_VOICE_ID = process.env.ELEVENLABS_VOICE_ID;
+
+export const CHATWOOT_ACCOUNT_ID = process.env.CHATWOOT_ACCOUNT_ID;
+export const CHATWOOT_TOKEN = process.env.CHATWOOT_TOKEN;
+export const CHATWOOT_ENDPOINT = process.env.CHATWOOT_ENDPOINT;
+
+
