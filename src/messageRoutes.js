833637e288419bcbaded47332e8dfb71ca88c5ad
Jose Gaspar <jgasparlopez29@correo.unicordoba.edu.co>
2024-10-10 18:00:25 -0500

    Refactorizar envío de mensajes con archivos adjuntos en messageRoutes.js

--- src/messageRoutes.js
+++ src/messageRoutes.js
@@ -102,11 +102,7 @@ export const setupRoutes = (provider, handleCtx) => {
 
           if (file) {
             console.log(`Este es el archivo adjunto...`, file.data_url);
-            if (media && media.includes(".oga")) {
-              await bot.sendAudio(number, media);
-            } else {
-              await bot.sendMessage(number, message, { media });
-            }
+            await bot.sendMessage(number, message, { media });
             res.end("ok");
             return;
           }
